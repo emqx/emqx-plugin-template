63db5ee64c9d21e378a02d0e9e852a199f110ddc
Feng <ery.lee@gmail.com>
2016-03-11 11:02:40 +0800

    Upgrade Hook

--- src/emqttd_plugin_template.erl
+++ src/emqttd_plugin_template.erl
@@ -26,74 +26,64 @@
 
 -export([on_client_subscribe/3, on_client_subscribe_after/3, on_client_unsubscribe/3]).
 
--export([on_message_publish/2, on_message_acked/3]).
+-export([on_message_publish/2, on_message_delivered/3, on_message_acked/3]).
 
 %% Called when the plugin application start
 load(Env) ->
+    emqttd:hook('client.connected', fun ?MODULE:on_client_connected/3, [Env]),
+    emqttd:hook('client.disconnected', fun ?MODULE:on_client_disconnected/3, [Env]),
+    emqttd:hook('client.subscribe', fun ?MODULE:on_client_subscribe/3, [Env]),
+    emqttd:hook('client.subscribe.after', fun ?MODULE:on_client_subscribe_after/3, [Env]),
+    emqttd:hook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/3, [Env]),
+    emqttd:hook('message.publish', fun ?MODULE:on_message_publish/2, [Env]),
+    emqttd:hook('message.delivered', fun ?MODULE:on_message_delivered/3, [Env]),
+    emqttd:hook('message.acked', fun ?MODULE:on_message_acked/3, [Env]).
 
-    emqttd_broker:hook('client.connected', {?MODULE, on_client_connected},
-                       {?MODULE, on_client_connected, [Env]}),
-
-    emqttd_broker:hook('client.disconnected', {?MODULE, on_client_disconnected},
-                       {?MODULE, on_client_disconnected, [Env]}),
-
-    emqttd_broker:hook('client.subscribe', {?MODULE, on_client_subscribe},
-                       {?MODULE, on_client_subscribe, [Env]}),
-
-    emqttd_broker:hook('client.subscribe.after', {?MODULE, on_client_subscribe_after},
-                       {?MODULE, on_client_subscribe_after, [Env]}),
-
-    emqttd_broker:hook('client.unsubscribe', {?MODULE, on_client_unsubscribe},
-                       {?MODULE, on_client_unsubscribe, [Env]}),
-
-    emqttd_broker:hook('message.publish', {?MODULE, on_message_publish},
-                       {?MODULE, on_message_publish, [Env]}),
-
-    emqttd_broker:hook('message.acked', {?MODULE, on_message_acked},
-                       {?MODULE, on_message_acked, [Env]}).
-
-on_client_connected(ConnAck, _Client = #mqtt_client{client_id = ClientId}, _Env) ->
-    io:format("client ~s connected, connack: ~w~n", [ClientId, ConnAck]).
+on_client_connected(ConnAck, Client = #mqtt_client{client_id = ClientId}, _Env) ->
+    io:format("client ~s connected, connack: ~w~n", [ClientId, ConnAck]),
+    {ok, Client}.
 
 on_client_disconnected(Reason, ClientId, _Env) ->
-    io:format("client ~s disconnected, reason: ~w~n", [ClientId, Reason]).
+    io:format("client ~s disconnected, reason: ~w~n", [ClientId, Reason]),
+    ok.
 
 %% should retain TopicTable
 on_client_subscribe(ClientId, TopicTable, _Env) ->
     io:format("client ~s will subscribe ~p~n", [ClientId, TopicTable]),
-    TopicTable.
+    {ok, TopicTable}.
    
 on_client_subscribe_after(ClientId, TopicTable, _Env) ->
-    io:format("client ~s subscribed ~p~n", [ClientId, TopicTable]).
+    io:format("client ~s subscribed ~p~n", [ClientId, TopicTable]),
+    {ok, TopicTable}.
     
 on_client_unsubscribe(ClientId, Topics, _Env) ->
     io:format("client ~s unsubscribe ~p~n", [ClientId, Topics]),
-    Topics.
+    {ok, Topics}.
 
 %% transform message and return
 on_message_publish(Message = #mqtt_message{topic = <<"$SYS/", _/binary>>}, _Env) ->
-    Message;
+    {ok, Message};
 
 on_message_publish(Message, _Env) ->
     io:format("publish ~s~n", [emqttd_message:format(Message)]),
-    Message.
+    {ok, Message}.
+
+on_message_delivered(ClientId, Message, _Env) ->
+    io:format("delivered to client ~s: ~s~n", [ClientId, emqttd_message:format(Message)]),
+    {ok, Message}.
 
 on_message_acked(ClientId, Message, _Env) ->
-    io:format("client ~s acked ~s", [ClientId, emqttd_message:format(Message)]).
+    io:format("client ~s acked: ~s~n", [ClientId, emqttd_message:format(Message)]),
+    {ok, Message}.
 
 %% Called when the plugin application stop
 unload() ->
-    emqttd_broker:unhook('client.connected', {?MODULE, on_client_connected}),
-
-    emqttd_broker:unhook('client.disconnected', {?MODULE, on_client_disconnected}),
-
-    emqttd_broker:unhook('client.subscribe', {?MODULE, on_client_subscribe}),
-
-    emqttd_broker:unhook('client.subscribe.after', {?MODULE, on_client_subscribe_after}),
-
-    emqttd_broker:unhook('client.unsubscribe', {?MODULE, on_client_unsubscribe}),
-
-    emqttd_broker:unhook('message.publish', {?MODULE, on_message_publish}),
-
-    emqttd_broker:unhook('message.acked', {?MODULE, on_message_acked}).
+    emqttd:unhook('client.connected', fun ?MODULE:on_client_connected/3),
+    emqttd:unhook('client.disconnected', fun ?MODULE:on_client_disconnected/3),
+    emqttd:unhook('client.subscribe', fun ?MODULE:on_client_subscribe/3),
+    emqttd:unhook('client.subscribe.after', fun ?MODULE:on_client_subscribe_after/3),
+    emqttd:unhook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/3),
+    emqttd:unhook('message.publish', fun ?MODULE:on_message_publish/2),
+    emqttd:unhook('message.acked', fun ?MODULE:on_message_acked/3),
+    emqttd:unhook('message.delivered', fun ?MODULE:on_message_delivered/3).
 
