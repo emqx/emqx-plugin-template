0cfbc71ecfe4558f106670588a41333cc3553010
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-05-11 17:10:25 +0200

    chore: add a script to check release version and git tag integrity

---
+++ check-vsn.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+REL_VSN="$(erl -noshell -eval '{ok, C} = file:consult("rebar.config"), {_, [{release, {_Name, Vsn}, _Apps} | _]} = lists:keyfind(relx, 1, C), io:format("~s\n", [Vsn]), halt(0).')"
+GIT_TAG="$(git describe --tags --exact 2>/dev/null)"
+
+if [ "$GIT_TAG" != '' ] && [ "$GIT_TAG" != "$REL_VSN" ]; then
+    echo "release version does not match git tag"
+    echo "tag=$GIT_TAG"
+    echo "vsn=$REL_VSN"
+    exit 1
+fi
+
