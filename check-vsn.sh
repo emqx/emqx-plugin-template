abc22898c58f8f1a83d13e8668ba448a429164a4
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-05-11 17:12:25 +0200

    ci: check version in ci

--- check-vsn.sh
+++ check-vsn.sh
@@ -2,8 +2,10 @@
 
 set -euo pipefail
 
+set -x
+
 REL_VSN="$(erl -noshell -eval '{ok, C} = file:consult("rebar.config"), {_, [{release, {_Name, Vsn}, _Apps} | _]} = lists:keyfind(relx, 1, C), io:format("~s\n", [Vsn]), halt(0).')"
-GIT_TAG="$(git describe --tags --exact 2>/dev/null)"
+GIT_TAG="$(git describe --tags --exact)"
 
 if [ "$GIT_TAG" != '' ] && [ "$GIT_TAG" != "$REL_VSN" ]; then
     echo "release version does not match git tag"
