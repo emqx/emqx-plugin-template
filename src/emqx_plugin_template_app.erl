f4dbacc0020d3a8606f79f40e11e318226c09b07
Stefan Strigler <stefan@strigler.de>
2023-06-14 18:06:25 +0200

    the same but as a rebar3 template

--- src/emqx_plugin_template_app.erl
+++ src/emqx_plugin_template_app.erl
@@ -1,20 +1,5 @@
-%%--------------------------------------------------------------------
-%% Copyright (c) 2020 EMQ Technologies Co., Ltd. All Rights Reserved.
-%%
-%% Licensed under the Apache License, Version 2.0 (the "License");
-%% you may not use this file except in compliance with the License.
-%% You may obtain a copy of the License at
-%%
-%%     http://www.apache.org/licenses/LICENSE-2.0
-%%
-%% Unless required by applicable law or agreed to in writing, software
-%% distributed under the License is distributed on an "AS IS" BASIS,
-%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-%% See the License for the specific language governing permissions and
-%% limitations under the License.
-%%--------------------------------------------------------------------
-
--module(emqx_plugin_template_app).
+{{=@@ @@=}}
+-module(@@name@@_app).
 
 -behaviour(application).
 
@@ -25,10 +10,9 @@
         ]).
 
 start(_StartType, _StartArgs) ->
-    {ok, Sup} = emqx_plugin_template_sup:start_link(),
-    emqx_plugin_template:load(application:get_all_env()),
+    {ok, Sup} = @@name@@_sup:start_link(),
+    @@name@@:load(application:get_all_env()),
     {ok, Sup}.
 
 stop(_State) ->
-    emqx_plugin_template:unload().
-
+    @@name@@:unload().
