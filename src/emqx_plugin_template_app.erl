60fc6e3059051e5aeb3b54d6349a201853bb3083
Stefan Strigler <stefan@strigler.de>
2023-06-19 14:23:03 +0200

    fix cli demo

--- src/emqx_plugin_template_app.erl
+++ src/emqx_plugin_template_app.erl
@@ -12,7 +12,10 @@
 start(_StartType, _StartArgs) ->
     {ok, Sup} = @@name@@_sup:start_link(),
     @@name@@:load(application:get_all_env()),
+
+    emqx_ctl:register_command(@@name@@, {@@name@@_cli, cmd}),
     {ok, Sup}.
 
 stop(_State) ->
+    emqx_ctl:unregister_command(@@name@@),
     @@name@@:unload().
---
+++ src/emqx_plugin_template_cli.erl
@@ -0,0 +1,12 @@
+{{=@@ @@=}}
+-module(@@name@@_cli).
+
+%% This is an example on how to extend `emqx ctl` with your own commands.
+
+-export([cmd/1]).
+
+cmd(["arg1", "arg2"]) ->
+    emqx_ctl:print("ok");
+
+cmd(_) ->
+    emqx_ctl:usage([{"cmd arg1 arg2", "cmd demo"}]).
